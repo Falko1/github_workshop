d923df9e9f22c4c275e1901f6d769221f55a88ce
Falko1 <Falko1@users.noreply.github.com>
2024-03-06 14:40:42 +0000

    ✅ Ready to clone and code.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for github_workshop package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("github_workshop", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="github_workshop",
+    version=read("github_workshop", "VERSION"),
+    description="Awesome github_workshop created by Falko1",
+    url="https://github.com/Falko1/github_workshop/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="Falko1",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["github_workshop = github_workshop.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from github_workshop.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "github_workshop"
